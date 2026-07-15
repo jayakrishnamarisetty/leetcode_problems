e22e9fe1a0feb2fe57c23ce7b265545467425313
M JAYA KRISHNA SAI TEJA <22A91A61G8@aec.edu.in>
2026-07-15 10:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (9.11%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,17 @@
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int rug = 0;   
+        int veda = 0;  
+
+        for (int i = 1; i <= n; i++) {
+            rug += 2 * i;
+            veda += 2 * i - 1;
+        }
+
+        return gcd(rug, veda);
+    }
+};
