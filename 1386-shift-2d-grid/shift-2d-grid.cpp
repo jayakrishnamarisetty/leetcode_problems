0bf65eef1ce100d9aefd5f4dae207956063aad15
M JAYA KRISHNA SAI TEJA <22A91A61G8@aec.edu.in>
2026-07-23 11:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (23.96%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int total = n * m;
+        
+        vector<vector<int>> rug(n, vector<int>(m));
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int veda = (i * m + j + k) % total;
+                
+                int rv = veda / m;
+                int cv = veda % m;
+                
+                rug[rv][cv] = grid[i][j];
+            }
+        }
+        
+        return rug;
+    }
+};
